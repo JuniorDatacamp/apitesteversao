4ed1e393313fb659ac683af5189d3166b447c026
Francisco Júnior <francisco.junior@datacamp.net.br>
2020-06-30 15:28:02 -0300

    validação cliente duplicado.

--- models/clientes.js
+++ models/clientes.js
@@ -218,7 +218,7 @@ exports.clienteDuplicado = function clienteDuplicado(cpf, cnpj){
 
     var sql, params;
 
-    if (cpf.length > 0){
+    if (!cnpj){
         sql = sqlClienteDuplicado;
         params = cpf;
     }else{
